d3ca80ce416e42ad387cb23192f88953416ae370
Kevin MENAGER <kevmenager@gmail.com>
2015-08-18 18:17:13 +0200

    Revert the Sample

--- app/src/main/java/com/github/glomadrian/materialanimatedswitch/sample/MainActivity.java
+++ app/src/main/java/com/github/glomadrian/materialanimatedswitch/sample/MainActivity.java
@@ -3,7 +3,7 @@ package com.github.glomadrian.materialanimatedswitch.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
+import android.widget.Toast;
 import com.github.glomadrian.materialanimatedswitch.MaterialAnimatedSwitch;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     mainView = findViewById(R.id.main);
-    /*materialAnimatedSwitch = (MaterialAnimatedSwitch) findViewById(R.id.pin);
+    materialAnimatedSwitch = (MaterialAnimatedSwitch) findViewById(R.id.pin);
     materialAnimatedSwitch2 = (MaterialAnimatedSwitch) findViewById(R.id.pin2);
     materialAnimatedSwitch3 = (MaterialAnimatedSwitch) findViewById(R.id.pin3);
     materialAnimatedSwitch4 = (MaterialAnimatedSwitch) findViewById(R.id.pin4);
+    masterSwitch = (MaterialAnimatedSwitch) findViewById(R.id.masterSwitch);
+
     materialAnimatedSwitch.setOnCheckedChangeListener(
         new MaterialAnimatedSwitch.OnCheckedChangeListener() {
           @Override public void onCheckedChanged(boolean isChecked) {
@@ -31,9 +33,5 @@ public class MainActivity extends AppCompatActivity {
             materialAnimatedSwitch2.toggle();
           }
         });
-    */
-    masterSwitch = (MaterialAnimatedSwitch) findViewById(R.id.masterSwitch);
-
-
   }
 }
