31f96bdebe50aa42fbe6a3a80246060448f9672a
Kevin MENAGER <kevmenager@gmail.com>
2015-08-18 13:13:49 +0200

    Revert "Add new options in xml"
    This reverts commit a6678e173be7dda719cacc987bd85eb55edf6e3e.

--- materialanimatedswitch/src/main/java/com/github/glomadrian/materialanimatedswitch/observer/BallMoveObservable.java
+++ materialanimatedswitch/src/main/java/com/github/glomadrian/materialanimatedswitch/observer/BallMoveObservable.java
@@ -7,25 +7,29 @@ import java.util.Observable;
  */
 public class BallMoveObservable extends Observable {
 
-    private int ballAnimationValue;
-    private int ballPosition;
+  private int ballAnimationValue;
+  private int ballPosition;
 
-    public void setBallAnimationValue(int val) {
-        this.ballAnimationValue = val;
-        setChanged();
-        notifyObservers();
-    }
+  public void setBallAnimationValue(int val) {
+    this.ballAnimationValue = val;
+    setChanged();
+    notifyObservers();
+  }
 
+  public void update() {
+    setChanged();
+    notifyObservers();
+  }
 
-    public int getBallAnimationValue() {
-        return ballAnimationValue;
-    }
+  public int getBallAnimationValue() {
+    return ballAnimationValue;
+  }
 
-    public void setBallPosition(int ballPosition) {
-        this.ballPosition = ballPosition;
-    }
+  public void setBallPosition(int ballPosition) {
+    this.ballPosition = ballPosition;
+  }
 
-    public int getBallPosition() {
-        return ballPosition;
-    }
+  public int getBallPosition() {
+    return ballPosition;
+  }
 }
